b2fc8c8131e8c5b2303e19a185bbcaacfa38c3df
Christoph Weegen <christophweegen@googlemail.com>
2018-05-06 11:53:52 +0200

    remove default name parameter from ConnectFour::Player#initialize

--- lib/connect_four/player.rb
+++ lib/connect_four/player.rb
@@ -2,7 +2,7 @@ module ConnectFour
   class Player
     attr_reader :id, :name
 
-    def initialize(id:, name: "AnonymousPlayer")
+    def initialize(id:, name:)
       @id = id
       @name = name
     end
