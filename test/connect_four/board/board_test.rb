bc33dc2becf75aaf2341dc3fa35c2864c5d1f824
Christoph Weegen <christophweegen@googlemail.com>
2018-05-13 12:29:02 +0200

    rewrite board tests as specs

--- test/connect_four/board/board_test.rb
+++ test/connect_four/board/board_test.rb
@@ -1,34 +1,34 @@
 require 'test_helper'
 
-class BoardTest < ConnectFourSpec
-  def setup
+describe ConnectFour::Board do
+  before do
     @players = []
     2.times do |i|
       @players << ConnectFour::Player.new(id: i, name: "Player")
     end
   end
 
-  def test_board_initialize_default_dimensions
+  it "has default dimensions if no dimensions given" do
     b = ConnectFour::Board.new(players: @players)
-    assert_equal b.width,  8
-    assert_equal b.height, 8
-    assert_equal b.column_headers, ('1'..'8').to_a
-    assert_equal b.rows.size, 8
+    expect(b.width).must_equal 8
+    expect(b.height).must_equal 8
+    expect(b.column_headers).must_equal(('1'..'8').to_a)
+    expect(b.rows.size).must_equal 8
   end
 
-  def test_board_with_dimensions_too_low
+  it "has minimum dimension if given dimensions too low" do
     b = ConnectFour::Board.new(width: 5, height: 5, players: @players)
-    assert_equal b.width,  8
-    assert_equal b.height, 8
-    assert_equal b.column_headers, ('1'..'8').to_a
-    assert_equal b.rows.size, 8
+    expect(b.width).must_equal 8
+    expect(b.height).must_equal 8
+    expect(b.column_headers).must_equal(('1'..'8').to_a)
+    expect(b.rows.size).must_equal 8
   end
 
-  def test_board_with_dimensions_too_high
+  it "has maximum dimensions if given dimensions are to high" do
     b = ConnectFour::Board.new(width: 100, height: 100, players: @players)
-    assert_equal b.width,          ConnectFour::Constants::MAX_BOARD_WIDTH
-    assert_equal b.height,         ConnectFour::Constants::MAX_BOARD_HEIGHT
-    assert_equal b.column_headers, ConnectFour::Constants::COLUMN_HEADERS
-    assert_equal b.rows.size,      ConnectFour::Constants::MAX_BOARD_HEIGHT
+    expect(b.width).must_equal          ConnectFour::Constants::MAX_BOARD_WIDTH
+    expect(b.height).must_equal         ConnectFour::Constants::MAX_BOARD_HEIGHT
+    expect(b.column_headers).must_equal ConnectFour::Constants::COLUMN_HEADERS
+    expect(b.rows.size).must_equal      ConnectFour::Constants::MAX_BOARD_HEIGHT
   end
 end
