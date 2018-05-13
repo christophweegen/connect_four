fb3d05d7aa655eb62157fc2e5c9bc51edc2e7cfe
Christoph Weegen <christophweegen@googlemail.com>
2018-05-13 12:08:49 +0200

    rewrite player_test as spec

--- test/connect_four/player/player_test.rb
+++ test/connect_four/player/player_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
-class PlayerTest < ConnectFourSpec
-  def test_player_has_id_and_name
+describe ConnectFour::Player do
+  it "has_an_id_and_name" do
     player = ConnectFour::Player.new(id: 1, name: "Player1")
-    assert player.id == 1
-    assert player.name == "Player1"
+    expect(player.id).must_equal 1
+    expect(player.name).must_equal "Player1"
   end
 
-  def test_player_id_is_required_kwarg
-    e = assert_raises(ArgumentError) { ConnectFour::Player.new(name: "Player") }
-    assert_equal e.message, "missing keyword: id"
+  it "has_id_as_a_required_kwarg" do
+    e = proc { ConnectFour::Player.new(name: "Player") }.must_raise(ArgumentError)
+    expect(e.message).must_equal "missing keyword: id"
   end
 end
