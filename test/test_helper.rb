1297cbb008b8e16dd922ffb7ebf4eef235d97546
Christoph Weegen <christophweegen@googlemail.com>
2018-05-16 10:45:37 +0200

    Incl. TestHelpers::UserInput in MiniTest::TestCase

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,11 +6,15 @@ require 'connect_four'
 require 'connect_four/player'
 require 'connect_four/board'
 require 'connect_four/constants'
+require 'connect_four/setup_prompt'
 
-require 'connect_four/test_helpers/board_generator'
+# require 'connect_four/test_helpers/board_generator'
+require 'connect_four/test_helpers/user_input'
 
 require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ConnectFourSpec < Minitest::Test
 end
+
+Minitest::Test.include ConnectFour::TestHelpers::UserInput
