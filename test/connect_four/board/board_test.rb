88ee98925fe7a7d0bef855465a59166f5b7ffc2f
Christoph Weegen <christophweegen@googlemail.com>
2018-05-16 12:38:56 +0200

    Rewrite specs as assertions

--- test/connect_four/board/board_test.rb
+++ test/connect_four/board/board_test.rb
@@ -10,25 +10,25 @@ describe ConnectFour::Board do
 
   it "has default dimensions if no dimensions given" do
     b = ConnectFour::Board.new(players: @players)
-    expect(b.width).must_equal 8
-    expect(b.height).must_equal 8
-    expect(b.column_headers).must_equal(('1'..'8').to_a)
-    expect(b.rows.size).must_equal 8
+    assert_equal 8, b.width
+    assert_equal 8, b.height
+    assert_equal ('1'..'8').to_a, b.column_headers
+    assert_equal 8, b.rows.size
   end
 
   it "has minimum dimension if given dimensions too low" do
     b = ConnectFour::Board.new(width: 5, height: 5, players: @players)
-    expect(b.width).must_equal 8
-    expect(b.height).must_equal 8
-    expect(b.column_headers).must_equal(('1'..'8').to_a)
-    expect(b.rows.size).must_equal 8
+    assert_equal 8, b.width
+    assert_equal 8, b.height
+    assert_equal ('1'..'8').to_a, b.column_headers
+    assert_equal 8, b.rows.size
   end
 
   it "has maximum dimensions if given dimensions are to high" do
     b = ConnectFour::Board.new(width: 100, height: 100, players: @players)
-    expect(b.width).must_equal          ConnectFour::Constants::MAX_BOARD_WIDTH
-    expect(b.height).must_equal         ConnectFour::Constants::MAX_BOARD_HEIGHT
-    expect(b.column_headers).must_equal ConnectFour::Constants::COLUMN_HEADERS
-    expect(b.rows.size).must_equal      ConnectFour::Constants::MAX_BOARD_HEIGHT
+    assert_equal ConnectFour::Constants::MAX_BOARD_WIDTH,  b.width
+    assert_equal ConnectFour::Constants::MAX_BOARD_HEIGHT, b.height
+    assert_equal ConnectFour::Constants::COLUMN_HEADERS,   b.column_headers
+    assert_equal ConnectFour::Constants::MAX_BOARD_HEIGHT, b.rows.size
   end
 end
--- test/connect_four/player/player_test.rb
+++ test/connect_four/player/player_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 describe ConnectFour::Player do
   it "has_an_id_and_name" do
     player = ConnectFour::Player.new(id: 1, name: "Player1")
-    expect(player.id).must_equal 1
-    expect(player.name).must_equal "Player1"
+    assert_equal 1, player.id
+    assert_equal "Player1", player.name
   end
 
   it "has_id_as_a_required_kwarg" do
-    e = proc { ConnectFour::Player.new(name: "Player") }.must_raise(ArgumentError)
-    expect(e.message).must_equal "missing keyword: id"
+    e = assert_raises(ArgumentError) { ConnectFour::Player.new(name: "Player") }
+    assert_equal "missing keyword: id", e.message
   end
 end
