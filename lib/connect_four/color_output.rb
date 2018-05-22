20ba7545ea124e001ea804ede5b2f62666cab419
Christoph Weegen <christophweegen@googlemail.com>
2018-05-22 15:03:35 +0200

    Edit COLOR_CODES

--- lib/connect_four/color_output.rb
+++ lib/connect_four/color_output.rb
@@ -1,14 +1,10 @@
 module ConnectFour
   module ColorOutput
-    COLOR_CODES = { 1 => "\033[1;31m",   # light red
-                    2 => "\033[1;32m",   # light green
-                    3 => "\033[1;33m",   # yellow
-                    4 => "\033[1;36m",   # light cyan
-                    5 => "\033[1;95m",   # magenta
-                    6 => "\033[1;34m",   # blue
-                    7 => "\033[1;37m",   # white
-                    8 => "\033[1;34m",   # light blue
-                    9 => "\033[1;37m" }  # light gray
+    COLOR_CODES = { 1 => "\033[1;31m",  # light red
+                    2 => "\033[1;32m",  # light green
+                    3 => "\033[1;33m",  # yellow
+                    4 => "\033[1;36m",  # light cyan
+                    5 => "\033[1;95m" } # magenta
   end
 
   String.class_eval do
