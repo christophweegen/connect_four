802825bf777fac173a515c7c6684a0977fd3c101
Christoph Weegen <christophweegen@googlemail.com>
2018-05-28 14:02:59 +0200

    Fix gem

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,13 +1,5 @@
-require_relative 'connect_four/player'
-require_relative 'connect_four/board'
-require_relative 'connect_four/board_setup'
-
 module ConnectFour
   def self.hello_world
     "hello world"
   end
 end
-
-board_setup = ConnectFour::BoardSetup.new
-board       = ConnectFour::Board.new(board_setup)
-board.play
